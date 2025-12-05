42b89cc6078c3edabfa22a1d5e130f22bbe609e4
Jas-1005 <jgmagadan@up.edu.ph>
2025-12-05 12:57:30 +0800

    Apply .gitignore

--- public/firebase.js
+++ public/firebase.js
@@ -2,17 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
-
-const firebaseConfig = {
-            apiKey: "AIzaSyDX2PT9q_Yx1PdPooSnKM2-K5cw4Z-1OnE",
-            authDomain: "cmsc-128-fullstack.firebaseapp.com",
-            projectId: "cmsc-128-fullstack",
-            storageBucket: "cmsc-128-fullstack.firebasestorage.app",
-            messagingSenderId: "726754132879",
-            appId: "1:726754132879:web:9d28c52f5b9bdc13be01fa",
-            measurementId: "G-CFCF9BFCMS",
-            databaseURL: "https://cmsc-128-fullstack-default-rtdb.firebaseio.com"
-        };
+import { firebaseConfig } from "../firebase_config.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
