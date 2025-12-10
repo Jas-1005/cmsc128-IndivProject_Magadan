47e082dd4b0cb37cf94e85d059533aca04d1c382
Jasmine Gargoles Magadan <jgmagadan@up.edu.ph>
2025-12-10 11:52:36 +0800

    firebase ignored

--- docs/firebase_template.js
+++ docs/firebase_template.js
@@ -1,3 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
+
+// Initialize Firebase
 const firebaseConfig = {
     apiKey: "__FIREBASE_API_KEY__",
     authDomain: "__FIREBASE_AUTH_DOMAIN__",
@@ -8,3 +14,11 @@ const firebaseConfig = {
     measurementId: "__FIREBASE_MEASUREMENT_ID__",
     databaseURL: "__FIREBASE_DATABASE_URL__"
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const DB = getFirestore(app);
+
+// console.log("Firebase initialized:", app);
+
+export {auth, DB};
